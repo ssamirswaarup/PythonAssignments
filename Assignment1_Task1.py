4ccd345fa3622267341750f1af68922a1bcc1eb1
ssamirswaarup <ssamirswaarup@gmail.com>
2025-07-21 22:02:39 +0530

    Python Assignment1 completed

---
+++ Assignment1_Task1.py
@@ -0,0 +1,7 @@
+# Task 1: Perform Basic Mathematical Operations
+num1=input("Enter the first number: ")
+num2=input("Enter the second number: ")
+print("Addition: ", int(num1) + int(num2))
+print("Subtraction: ", int(num1) - int(num2))
+print("Multiplication: ", int(num1) * int(num2))
+print("Division: ", int(num1) / int(num2))
---
+++ Assignment1_Task2.py
@@ -0,0 +1,4 @@
+# Task 2: Create a Personalized Greeting
+firstname= input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+print("Hello, " + firstname + " " + lastname + "! Welcome to the Python program.")
